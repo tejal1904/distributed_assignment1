c165e5976a227f7710915ae0fa04a2e66fc29aef
Tejal Jadhav <tjadhav@student.unimelb.edu.au>
2018-04-20 19:17:14 +1000

    New clientpojo class

---
+++ Multiserver/src/activitystreamer/client/ClientPojo.java
@@ -0,0 +1,42 @@
+package activitystreamer.client;
+
+import java.net.Socket;
+
+public class ClientPojo {
+    String username;
+    String secret;
+    Socket socket;
+
+    public ClientPojo() {
+    }
+
+    public ClientPojo(String username, String secret, Socket socket) {
+        this.username = username;
+        this.secret = secret;
+        this.socket = socket;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+}
