2bb0a5d3f590519824fb5a167db22082ff61a213
sindhu <sindhu@DESKTOP-I8G39IR.gateway>
2018-04-22 00:39:59 +1000

    Client changes in Socket connection

--- Multiserver/src/activitystreamer/client/ClientSkeleton.java
+++ Multiserver/src/activitystreamer/client/ClientSkeleton.java
@@ -5,6 +5,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -20,56 +21,61 @@ public class ClientSkeleton extends Thread {
 	private static final Logger log = LogManager.getLogger();
 	private static ClientSkeleton clientSolution;
 	private TextFrame textFrame;
-	
-	public static ClientSkeleton getInstance(){
-		if(clientSolution==null){
+	private PrintWriter outwriter;
+	private BufferedReader inReader;
+
+	public static ClientSkeleton getInstance() {
+		if (clientSolution == null) {
 			clientSolution = new ClientSkeleton();
 		}
 		return clientSolution;
 	}
-	
-	public ClientSkeleton(){		
+
+	public ClientSkeleton() {
 		textFrame = new TextFrame();
-		start();
-	}	
-	
-	@SuppressWarnings("unchecked")
-	public void sendActivityObject(JSONObject activityObj){
-		
-		try(Socket socket = new Socket(Settings.getRemoteHostname(), Settings.getLocalPort());){
-			// Output and Input Stream
-			DataInputStream in = new DataInputStream(socket.
-					getInputStream());
-		    DataOutputStream out = new DataOutputStream(socket.
-		    		getOutputStream());
-		    PrintWriter outwriter = new PrintWriter(out, true);
-		    BufferedReader inReader = new BufferedReader( new InputStreamReader(in));
-		    outwriter.println(activityObj.toString());
-	    	outwriter.flush();    
-			String message = inReader.readLine();
-			JSONParser parser = new JSONParser();
-			JSONObject outputJson = (JSONObject) parser.parse(message);
-			
-			System.out.println("message Received from server: "+message);
-			textFrame.setOutputText(outputJson);
-		    
+		try {
+			Socket socket = new Socket(Settings.getRemoteHostname(), Settings.getLocalPort());
+			inReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			outwriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+
+		start();
+	}
+
+	public void sendActivityObject(JSONObject activityObj) {
+		outwriter.println(activityObj.toString());
+		outwriter.flush();
+	}
+
+	public void disconnect() {
+
+	}
+
+	public void run() {
+		String message;
+		try {
+			while ((message = inReader.readLine()) != null) {
+				JSONParser parser = new JSONParser();
+				JSONObject outputJson = (JSONObject) parser.parse(message);
+
+				System.out.println("message Received from server: " + message);
+				textFrame.setOutputText(outputJson);
+			}
+			;
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
-		
-	}	
-	
-	public void disconnect(){
-		
-	}	
-	
-	public void run(){		
+		}
 
 	}
-	
+
 }
