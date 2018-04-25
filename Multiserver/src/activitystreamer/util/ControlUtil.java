594543a56e99f9f4cf3fc68dac72e0a9619135bf
Tejal Jadhav <tjadhav@student.unimelb.edu.au>
2018-04-25 20:44:39 +1000

    registration changes

--- Multiserver/src/activitystreamer/util/ControlUtil.java
+++ Multiserver/src/activitystreamer/util/ControlUtil.java
@@ -213,7 +213,7 @@ public class ControlUtil {
 		if (!controlInstance.getRegisteredClients().containsKey(username)) {
 			controlInstance.addToBeRegisteredClients(msg, connection);
 			lockAllowedCount.put(username, 0);
-			if (serverList.size() < 2) {
+			if (serverList.size() == 0) {
 				resultOutput.put("command", "REGISTER_SUCCESS");
 				resultOutput.put("info", "register success for " + username);
 				connection.writeMsg(resultOutput.toJSONString());
